3bf98298670c602260bef02cd21a5def846fd6aa
Shivu1920 <global-config@example.com>
2022-11-01 21:30:11 +0530

    UI-4 Chats List

--- App.js
+++ App.js
@@ -1,27 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatListItem from './src/components/ChatListItem';
+//import ChatListItem from './src/components/ChatListItem';
+import ChatsScreen from './src/screens/ChatsScreen';
 
-const chat = {
-  id: "1",
-  user: {
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-    name: "Lukas",
-  },
-  lastMessage: {
-    text: "Oke", 
-    createdAt: "07:30",
-  },
-};
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ChatsScreen />
       
-      <ChatListItem chat={chat} />
-      <ChatListItem />
-
       <StatusBar style="auto" />
     </View>
   );
@@ -31,7 +18,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 50,
+
   },
 });
---
+++ src/screens/ChatsScreen.js
@@ -0,0 +1,15 @@
+// CHATSSCREEN.JS 
+import { View, Text, FlatList } from 'react-native'
+import chats from '../../assets/data/chats.json';
+import ChatListItem from '../components/ChatListItem';
+const ChatsScreen = () => {
+  return (
+    <FlatList 
+        data={chats} 
+        renderItem={({item}) => <ChatListItem chat={item}/>}
+    />
+  );
+};
+
+export default ChatsScreen;
+
